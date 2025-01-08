427eae54d4ed56ef8382fba051d1a9d77e271d25
Christian Bruneau <christian.bruneau777@gmail.com>
2025-01-07 21:06:24 -0500

    selection sort

---
+++ src/ca/cricri/algorithm/sorting/SelectionSort.java
@@ -0,0 +1,36 @@
+package ca.cricri.algorithm.sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+
+    public static void main(String[] args) {
+        int[] arrayToSort = {5, 12, 23, 51, 1, 23, 1, 4, 6, 156, 6, 3};
+
+        SelectionSort ss = new SelectionSort();
+        System.out.println("sorted   array=" + Arrays.toString(ss.sort(arrayToSort)));
+        System.out.println("validate sorted array=" +
+        Arrays.toString(Arrays.stream(arrayToSort).sorted().toArray()));
+    }
+
+    int[] sort(int[] arrayToSort) {
+        for (int i = 0; i<arrayToSort.length; i++) {
+            int min = Integer.MAX_VALUE;
+            int minPosition = -1;
+            for (int j = i; j<arrayToSort.length; j++) {
+                if (arrayToSort[j] < min) {
+                    min = arrayToSort[j];
+                    minPosition = j;
+                }
+            }
+            // swap
+            int tmp = arrayToSort[i];
+            arrayToSort[i] = arrayToSort[minPosition];
+            arrayToSort[minPosition] = tmp;
+        }
+        return arrayToSort;
+    }
+}
+
+
